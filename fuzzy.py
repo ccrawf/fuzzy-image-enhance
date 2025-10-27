e755accabd946ea1a0dc8246393787897c786c54
ccrawf <crawfordc14@yahoo.com>
2025-10-27 14:33:46 -0400

    updated membership functions (more final)

--- fuzzy.py
+++ fuzzy.py
@@ -8,6 +8,7 @@ from skfuzzy.control import Antecedent
 from skfuzzy.control import Consequent
 from skfuzzy.defuzzify import defuzz
 from skfuzzy.membership import trapmf
+import matplotlib.pyplot as plt
 # from skfuzzy.image import nmse
 
 # Read import image and convert array to HSV
@@ -46,7 +47,7 @@ def inferenceSystem(controller):
     
 
 # Variables
-hueRange = np.arange(0,180, 1) # hue range: 0-179
+hueRange = np.arange(0,360, 1) # hue range: 0-360
 svRange = np.arange(0, 256, 1) # sat/value range: 0-255
 
 # Fuzzy Variables
@@ -59,23 +60,43 @@ saturationFinal = Consequent(svRange, 'saturationFinal')
 valueFinal = Consequent(svRange, 'valueFinal')
 
 # Membership Functions (Input)
-hueInitial['low'] = trapmf(hueRange, [0,0,45,75])
-hueInitial['medium'] = trapmf(hueRange, [45,75,105,135])
-hueInitial['high'] = trapmf(hueRange, [105,135,179,179])
+hueInitial['red'] = trapmf(hueRange, [0,0,10,15])
+hueInitial['brown'] = trapmf(hueRange, [10,15,18,23])
+hueInitial['orange'] = trapmf(hueRange, [18,23,35,40])
+hueInitial['yellow'] = trapmf(hueRange, [35,40,60,90])
+hueInitial['green'] = trapmf(hueRange, [70,90,130,160])
+hueInitial['cyan'] = trapmf(hueRange, [140,160,190,210])
+hueInitial['blue'] = trapmf(hueRange, [190,210,240,270])
+hueInitial['purple'] = trapmf(hueRange, [255,270,290,305])
+hueInitial['pink'] = trapmf(hueRange, [295,305,330,345])
+hueInitial['red2'] = trapmf(hueRange, [330,345,359,359])
 
-saturationInitial['dull'] = trapmf(svRange, [0,0,70,90])
-saturationInitial['moderate'] = trapmf(svRange, [70,90,165,185])
-saturationInitial['vivid'] = trapmf(svRange, [165,185,255,255])
+saturationInitial['dull'] = trapmf(svRange, [0,0,28,46])
+saturationInitial['m_dull'] = trapmf(svRange, [28,46,64,82])
+saturationInitial['moderate'] = trapmf(svRange, [64,82,138,156])
+saturationInitial['m_vivid'] = trapmf(svRange, [138,156,173,191])
+saturationInitial['vivid'] = trapmf(svRange, [173,191,255,255])
 
-valueInitial['smooth'] = trapmf(svRange, [0,0,70,90])
-valueInitial['medium'] = trapmf(svRange, [70,90,165,185])
-valueInitial['sharp'] = trapmf(svRange, [165,185,255,255])
+# 0 = black
+# 255 = no darkness
+valueInitial['smooth'] = trapmf(svRange, [0,0,55,70])
+valueInitial['m_smooth'] = trapmf(svRange, [55,70,95,110])
+valueInitial['medium'] = trapmf(svRange, [95,110,140,155])
+valueInitial['m_sharp'] = trapmf(svRange, [140,155,180,195])
+valueInitial['sharp'] = trapmf(svRange, [180,195,255,255])
 
 # Membership Functions (Output)
 # Note: output functions are equivalent to input
-hueFinal['low'] = trapmf(hueRange, [0,0,45,75])
-hueFinal['medium'] = trapmf(hueRange, [45,75,105,135])
-hueFinal['high'] = trapmf(hueRange, [105,135,179,179])
+hueFinal['red'] = trapmf(hueRange, [0,0,10,15])
+hueFinal['brown'] = trapmf(hueRange, [10,15,18,23])
+hueFinal['orange'] = trapmf(hueRange, [18,23,35,40])
+hueFinal['yellow'] = trapmf(hueRange, [35,40,60,90])
+hueFinal['green'] = trapmf(hueRange, [70,90,130,160])
+hueFinal['cyan'] = trapmf(hueRange, [140,160,190,210])
+hueFinal['blue'] = trapmf(hueRange, [190,210,240,270])
+hueFinal['purple'] = trapmf(hueRange, [255,270,290,305])
+hueFinal['pink'] = trapmf(hueRange, [295,305,330,345])
+hueFinal['red2'] = trapmf(hueRange, [330,345,359,359])
 
 saturationFinal['dull'] = trapmf(svRange, [0,0,70,90])
 saturationFinal['moderate'] = trapmf(svRange, [70,90,165,185])
@@ -100,6 +121,12 @@ image, width, height = importImage("./random-colors-2.png")
 # Inference System
 controller = ControlSystem(ruleset)
 saturation_map, value_map = inferenceSystem(controller)
+
+hueInitial.view()
+saturationInitial.view()
+valueInitial.view()
+
+plt.show()
 
 for i, pixel in enumerate(image):
     h, s, v = pixel
